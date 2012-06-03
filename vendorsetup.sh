547e78aae474a3316fed08bd45dab4893f33c00a
dhacker29 <davidhackerdvm@gmail.com>
2012-06-02 21:26:57 -0500

    add eng lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_targa-userdebug
+add_lunch_combo full_targa-eng
 add_lunch_combo cyanogen_targa-userdebug
