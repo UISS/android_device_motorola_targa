42b35e0274a13d7e020d417a04d07892222d3a0d
dhacker29 <davidhackerdvm@gmail.com>
2012-09-10 01:03:54 -0500

    Allow kexec and stock-based to build from same
    device/vendor and add multi-product support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,4 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_targa.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_targa.mk
+ifeq ($(TARGET_PRODUCT),aokp_targa)
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
+endif
