fd7545157b4da2285b7717132de2d3f1d3e3d717
Hashcode <hashcode0f@gmail.com>
2013-03-08 10:58:24 -0800

    allow stock-based kexec build
    Change-Id: Ic9880e53f300741e6d92d309c47637c5f7929953

--- device.mk
+++ device.mk
@@ -26,13 +26,13 @@ PRODUCT_PACKAGES += \
     libreference-cdma-sms
 
 # Kexec files and ti ducati or rootfs files
-ifeq ($(BOARD_USES_KEXEC),true)
+#    $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
+#ifeq ($(BOARD_USES_KEXEC),true)
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
     $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
     $(OUT)/kernel:system/etc/kexec/kernel
-endif
+#endif
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
@@ -42,12 +42,12 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab
 
 # copy all kernel modules under the "modules" directory to system/lib/modules
-ifneq ($(BOARD_USES_KEXEC),true)
-PRODUCT_COPY_FILES += $(shell \
-    find device/motorola/targa/modules -name '*.ko' \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-endif
+#ifneq ($(BOARD_USES_KEXEC),true)
+#PRODUCT_COPY_FILES += $(shell \
+#    find device/motorola/targa/modules -name '*.ko' \
+#    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
+#    | tr '\n' ' ')
+#endif
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/proprietary/vzw/verizon.mk)
