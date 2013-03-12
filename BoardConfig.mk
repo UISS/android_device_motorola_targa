3720577884efc977098b9ddb7694d58347fd0856
Hashcode <hashcode0f@gmail.com>
2013-03-12 11:05:32 -0700

    device build changes
    Change-Id: I88b1685f2a9fd53148281036aa4b0cd2db0e9c3a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,5 @@
 # inherit from common
-ifeq ($(BOARD_USES_KEXEC),true)
 -include device/motorola/omap4-kexec-common/BoardConfigCommon.mk
-else
--include device/motorola/omap4-common/BoardConfigCommon.mk
-endif
 
 # inherit from the proprietary version
 -include vendor/motorola/targa/BoardConfigVendor.mk
@@ -12,19 +8,11 @@ endif
 TARGET_BOOTLOADER_BOARD_NAME := targa
 
 # Kernel
-ifeq ($(BOARD_USES_KEXEC),true)
-BOARD_KERNEL_CMDLINE := root=/dev/ram0 rw mem=1024M@0x80000000 console=ttyO2,115200n8 init=/init ip=off mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(webtop),p25(userdata),p26(emstorage) mot_sst=1 androidboot.bootloader=0x0A72
-else
-BOARD_KERNEL_CMDLINE := omap_wdt.timer_margin=60 oops=panic console=/dev/null rw mem=1048572K@0x80000000 vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(webtop),p25(userdata),p26(emstorage) androidboot.bootloader=0x0A72
-endif
+BOARD_KERNEL_CMDLINE := root=/dev/ram0 rw mem=1023M@0x80000000 console=null vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(webtop),p25(userdata),p26(emstorage) mot_sst=1 androidboot.bootloader=0x0A72
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_PAGE_SIZE := 0x4096
 
 # Kernel Build
-ifeq ($(BOARD_USES_KEXEC),true)
 TARGET_KERNEL_SOURCE := kernel/motorola/omap4-kexec-common
-TARGET_KERNEL_CONFIG := mapphone_targa_jb_defconfig
-else
-TARGET_KERNEL_SOURCE := kernel/motorola/omap4-common
-TARGET_KERNEL_CONFIG := mapphone_defconfig
-endif
+TARGET_KERNEL_CONFIG := mapphone_targa_defconfig
+
--- device.mk
+++ device.mk
@@ -1,11 +1,7 @@
 #
 # This is the product configuration for a full targa
 #
-ifeq ($(BOARD_USES_KEXEC),true)
 $(call inherit-product, device/motorola/omap4-kexec-common/common.mk)
-else
-$(call inherit-product, device/motorola/omap4-common/common.mk)
-endif
 
 DEVICE_FOLDER := device/motorola/targa
 
@@ -14,10 +10,10 @@ DEVICE_FOLDER := device/motorola/targa
 
 # Hardware HALs
 PRODUCT_PACKAGES += \
+    power.targa \
     camera.targa \
-    lights.targa
-
-#    hwcomposer.targa
+    lights.targa \
+    hwcomposer.targa
 
 # Modem
 PRODUCT_PACKAGES += \
@@ -25,35 +21,15 @@ PRODUCT_PACKAGES += \
     Stk \
     libreference-cdma-sms
 
-# Kexec files and ti ducati or rootfs files
-ifeq ($(BOARD_USES_KEXEC),true)
+# Kexec rootfs files
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
     $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
     $(OUT)/kernel:system/etc/kexec/kernel
-endif
-
-# Prebuilts
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    $(DEVICE_FOLDER)/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    $(DEVICE_FOLDER)/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab
-
-# copy all kernel modules under the "modules" directory to system/lib/modules
-ifneq ($(BOARD_USES_KEXEC),true)
-PRODUCT_COPY_FILES += $(shell \
-    find device/motorola/targa/modules -name '*.ko' \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-endif
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/proprietary/vzw/verizon.mk)
 $(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor.mk)
-ifneq ($(BOARD_USES_KEXEC),true)
-$(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor-pvr.mk)
-$(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor-stock-camera.mk)
 $(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor-stock-ducati.mk)
-endif
+$(call inherit-product-if-exists, vendor/motorola/omap4-common/common-vendor-pvr.mk)
+
