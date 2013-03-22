94b8e09903c360fbf97157bbd9045450b79b32ad
dhacker29 <davidhackerdvm@gmail.com>
2013-03-22 01:55:44 -0500

    Add ubuntu .conf

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml \
     $(DEVICE_FOLDER)/vold.fstab:system/etc/vold.fstab
 
+# Add ubuntu conf
+PRODUCT_COPY_FILES += \
+    $(DEVICE_FOLDER)/targa.conf:system/etc/ubuntu-session.d/targa.conf
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/common-vendor-pvr.mk)
